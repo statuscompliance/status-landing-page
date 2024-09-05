5810c87e3245d096e460632171785e0c45922eac
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-09-05 14:10:28 +0200

    fix: Add YT iframe

--- components/hero.tsx
+++ components/hero.tsx
@@ -38,12 +38,10 @@ export default function Hero() {
           {/* Hero image */}
           <ModalVideo
             thumb={VideoThumb}
-            thumbWidth={768}
-            thumbHeight={432}
+            thumbWidth={1024}
+            thumbHeight={576}
             thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} />
+            videoUrl="https://www.youtube.com/embed/JZkFzZzz-qg" />
 
         </div>
 
--- components/modal-video.tsx
+++ components/modal-video.tsx
@@ -10,9 +10,7 @@ interface ModalVideoProps {
   thumbWidth: number
   thumbHeight: number
   thumbAlt: string
-  video: string
-  videoWidth: number
-  videoHeight: number
+  videoUrl: string // URL to the video
 }
 
 export default function ModalVideo({
@@ -20,35 +18,35 @@ export default function ModalVideo({
   thumbWidth,
   thumbHeight,
   thumbAlt,
-  video,
-  videoWidth,
-  videoHeight,
+  videoUrl,
 }: ModalVideoProps) {
   const [modalOpen, setModalOpen] = useState<boolean>(false)
-  const videoRef = useRef<HTMLVideoElement>(null)
+  const iframeRef = useRef<HTMLIFrameElement>(null)
 
   return (
     <div>
-
       {/* Video thumbnail */}
       <div>
         <div className="relative flex justify-center mb-8" data-aos="zoom-y-out" data-aos-delay="450">
           <div className="flex flex-col justify-center">
             <Image src={thumb} width={thumbWidth} height={thumbHeight} alt={thumbAlt} />
           </div>
-          <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={() => { setModalOpen(true) }}>
+          <button
+            className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg"
+            onClick={() => setModalOpen(true)}
+          >
             <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
               <path d="M10 17l6-5-6-5z" />
             </svg>
-            <span className="ml-3">Check a demo of our prototype(3 min)</span>
+            <span className="ml-3">Check a demo of our prototype (4 min)</span>
           </button>
         </div>
       </div>
       {/* End: Video thumbnail */}
 
-      <Transition show={modalOpen} as={Fragment} afterEnter={() => videoRef.current?.play()}>
-        <Dialog initialFocus={videoRef} onClose={() => setModalOpen(false)}>
+      <Transition show={modalOpen} as={Fragment}>
+        <Dialog initialFocus={iframeRef} onClose={() => setModalOpen(false)}>
 
           {/* Modal backdrop */}
           <Transition.Child
@@ -69,16 +67,22 @@ export default function ModalVideo({
             enter="transition ease-out duration-200"
             enterFrom="opacity-0 scale-95"
             enterTo="opacity-100 scale-100"
-            leave="ttransition ease-out duration-200"
-            leaveFrom="oopacity-100 scale-100"
+            leave="transition ease-out duration-200"
+            leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="max-w-6xl mx-auto h-full flex items-center">
+            <div className="max-w-4xl max-h-4xl w-full mx-auto h-full flex items-center">
               <Dialog.Panel className="w-full max-h-full aspect-video bg-black overflow-hidden">
-                <video ref={videoRef} width={videoWidth} height={videoHeight} loop controls>
-                  <source src={video} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
+                {/* Updated styles for iframe */}
+                <iframe
+                  ref={iframeRef}
+                  width="100%"
+                  height="100%"
+                  src={videoUrl}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="w-full h-[315px] sm:h-[400px] md:h-[500px] lg:h-[505px]"
+                />
               </Dialog.Panel>
             </div>
           </Transition.Child>
